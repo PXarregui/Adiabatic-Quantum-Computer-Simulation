d3a7e516b490ca335e5322921e7dbadee732f7e7
Titoxabat98 <62847812+Titoxabat98@users.noreply.github.com>
2020-08-05 19:11:34 +0200

    Number of particles added
    The file containing the initial parameters is opened and the number of particles present in the simulation is assigned.

--- Simulation.py
+++ Simulation.py
@@ -5,4 +5,8 @@ import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 
 
-
+parameters=open("Simulation_parameters.txt","r")
+all_lines=parameters.readlines()
+parameters.close()
+#nspin determines the number of particles used in the simulation
+nspin=int(all_lines[0])
