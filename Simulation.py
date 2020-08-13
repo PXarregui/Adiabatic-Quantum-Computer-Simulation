93a018368da3066dc097e32bcb85c46b02298923
Titoxabat98 <62847812+Titoxabat98@users.noreply.github.com>
2020-08-13 23:02:44 +0200

    Add J_ij generating function and testing
    Write the J_ij coefficient generating function and the corresponding test.

--- Simulation.py
+++ Simulation.py
@@ -36,7 +36,40 @@ def coefficient_hi_generator(number_of_particles,random_or_not):
                 i=i+1
             return(hi)  
  
-    
+#This function generates Jij coefficients in order to generate the later Hamiltonians       
+def coefficient_Jij_generator(number_of_particles,random_or_not):
+    if abs(number_of_particles)>8 or abs(number_of_particles)==0:
+        sys.exit("You must insert as number of particles a positive integer lower than 8 and higher than 0.")
+    elif random_or_not not in [0,1]:
+        sys.exit("The second line of the file containing the parameters must be 0 or 1.")
+    else:
+        if random_or_not==0:
+            #Create both Jij and hi arrays with 0s to give them value later
+            Jij=np.zeros((number_of_particles,number_of_particles))
+            i=0
+            while i<number_of_particles:
+                j=0
+                while j<number_of_particles:
+                    if i<j:
+                        Jij[i,j]=float(input("Introduce the element J{}{}: ".format(i+1,j+1)))
+                        Jij[j,i]=Jij[i,j]
+                    j=j+1
+                i=i+1
+            return(Jij)
+        elif random_or_not==1:
+            #Create both Jij and hi arrays with 0s to give them value later
+            Jij=np.zeros((number_of_particles,number_of_particles))
+            i=0
+            while i<number_of_particles:
+                j=0
+                random.seed()
+                while j<number_of_particles:
+                    if i<j:#The coupling coefficient is 0 if the index is equal
+                        Jij[i,j]=random.random()
+                        Jij[j,i]=Jij[i,j]
+                    j=j+1 
+                i=i+1
+            return(Jij)   
     
     
     
--- test_Simulation.py
+++ test_Simulation.py
@@ -10,5 +10,18 @@ def test_coefficient_hi_generator():
         assert isinstance(Simulation.coefficient_hi_generator(3, 1)[i], float)
         i+=1
 
-
+#Test if the Jij coefficient matrix is symmetric with 0s in the diagonal and real numbers elsewhere.
+def test_matrix_coefficient_Jij_generator():
+    i=0
+    generated_matrix=Simulation.coefficient_Jij_generator(3, 1)
+    while i<3:
+        j=0
+        while j<3:
+            if i==j:
+                assert generated_matrix[i,j]==0.
+            else:
+                assert isinstance(generated_matrix[i,j], float)
+                assert generated_matrix[i,j]==generated_matrix[j,i]
+            j+=1
+        i+=1
         
