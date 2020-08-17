4d27aff04d1cd09466182b4d466af3bc4a565bef
Titoxabat98 <62847812+Titoxabat98@users.noreply.github.com>
2020-08-17 09:14:49 +0200

    Split the tests.
    Split the tests of the coeffcicient generator functions into multiple simpler tests.

--- test_Simulation.py
+++ test_Simulation.py
@@ -2,20 +2,29 @@ import Simulation
 import numpy
 import pytest
 
-#Test the hi coefficient generator function to see if the return value is a properly shaped numpy array of real numbers.
-def test_coefficient_hi_generator():
+# Tests concerning coefficient_hi_generator()
+
+#Test the hi coefficient generator function to see if the return value is a properly shaped numpy array.
+def test_shape_coefficient_hi_generator():
     assert numpy.shape(Simulation.coefficient_hi_generator(3, 1))==(3,)
+ 
+#Test the hi coefficient generator function to see if the return value is an array of real numbers.
+def test_real_coefficient_Jij_generator():
     i=0
     while i<3:
         assert isinstance(Simulation.coefficient_hi_generator(3, 1)[i], float)
         i+=1
 
+# -----------------------------------------------------------------------------
+        
+# Tests concerning coefficient_Jij_generator()
+
 #Test the Jij generator function to see if the matrix has the apropriate dimension. 
 def test_shape_coefficient_Jij_generator():
     assert numpy.shape(Simulation.coefficient_Jij_generator(3, 1))==(3,3)
 
-#Test if the Jij coefficient matrix is symmetric with 0s in the diagonal and real numbers elsewhere.
-def test_matrix_coefficient_Jij_generator():
+#Test if the Jij coefficient matrix has 0s in the diagonal.
+def test_matrix_diagonal_coefficient_Jij_generator():
     i=0
     generated_matrix=Simulation.coefficient_Jij_generator(3, 1)
     while i<3:
@@ -23,9 +32,35 @@ def test_matrix_coefficient_Jij_generator():
         while j<3:
             if i==j:
                 assert generated_matrix[i,j]==0.
-            else:
+            j+=1
+        i+=1
+         
+#Test if the Jij coefficient matrix is composed of real numbers elsewhere.
+def test_matrix_real_coefficient_Jij_generator():
+    i=0
+    generated_matrix=Simulation.coefficient_Jij_generator(3, 1)
+    while i<3:
+        j=0
+        while j<3:
+            if i!=j:
                 assert isinstance(generated_matrix[i,j], float)
                 assert generated_matrix[i,j]==generated_matrix[j,i]
             j+=1
         i+=1
+ 
+#Test if the Jij coefficient matrix is symmetric.
+def test_symmetry_coefficient_Jij_generator():
+    i=0
+    generated_matrix=Simulation.coefficient_Jij_generator(3, 1)
+    while i<3:
+        j=0
+        while j<3:
+            if i!=j:
+                assert generated_matrix[i,j]==generated_matrix[j,i]
+            j+=1
+        i+=1      
+        
+# -----------------------------------------------------------------------------        
+        
+        
         
